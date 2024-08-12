7c35e954bbf452c8fe1b7fe2ccdf729c60d5bbad
pclank <31415965+pclank@users.noreply.github.com>
2024-08-13 01:32:54 +0300

    opencl integrated.

--- Glitter/Headers/glitter.hpp
+++ Glitter/Headers/glitter.hpp
@@ -11,10 +11,11 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <CL/cl.hpp>
 
 // Reference: https://github.com/nothings/stb/blob/master/stb_image.h#L4
 // To use stb_image, add this in *one* C++ source file.
-//     #define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
 // Define Some Constants
